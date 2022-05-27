1f569c29229a7190f355a1ba1fd37acc19380fb3
Adrian <u201718169@upc.edu.pe>
2022-05-26 23:24:35 -0500

    feature: feature membership card and membership-list

---
+++ src/beat-club/components/memberships-card/membership-card-list.component.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>Trending Tracks</h1>
+    <div class="flex flex-wrap justify-content-center card-container gap-3">
+      <membership-card
+        v-for="songData in membershipData"
+        :key="songData.id"
+        :membershipData="songData"
+        style="max-width: 300px"
+      ></membership-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import membershipCard from "/src/beat-club/components/memberships-card/membership-card.component.vue";
+import { BeatClubApiServices } from "../../services/beat-club-api.services.js";
+export default {
+  components: {
+    membershipCard,
+  },
+  name: "membership-card-list",
+  data() {
+    return {
+      membershipData: [],
+      challengesService: undefined,
+    };
+  },
+  created() {
+    this.challengesService = new BeatClubApiServices();
+    this.getAllCards();
+  },
+  methods: {
+    getAllCards() {
+      this.challengesService.getMemberships().then((response) => {
+        this.membershipData = response.data.filter((item) => item.id);
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.grid {
+  display: flex;
+  justify-content: center;
+  font-family: "Poppins", sans-serif;
+}
+h1 {
+  padding-top: 10px;
+  /*padding-left: 165px;*/
+  margin-bottom: 20px;
+}
+h1,
+p {
+  font-family: "Poppins", sans-serif;
+}
+
+p {
+  text-align: center;
+}
+</style>
---
+++ src/beat-club/components/memberships-card/membership-card.component.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <pv-card class="songCard">
+      <template #header>
+        <img alt="user header" :src="membershipData.urlToImage" />
+      </template>
+      <template #content>
+        {{ membershipData.title }}
+        <p>{{ membershipData.description }}</p>
+        <p>{{ membershipData.userId }}</p>
+      </template>
+    </pv-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "membership-card.component",
+  props: {
+    membershipData: Object,
+  },
+};
+</script>
+
+<style scoped>
+.songCard {
+  background-color: #161d21;
+  color: white;
+}
+</style>
