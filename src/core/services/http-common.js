22fb6d9c3a4b6ee7946081f2324c2e7ad4ded4ae
adrianesqueiros <u201718169@upc.edu.pe>
2022-06-09 22:45:52 -0500

    update my-json-server.typicode.com

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+  baseURL:
+    "https://my-json-server.typicode.com/BitForce-BeatClub/Beat-Club-Frontend",
   headers: { "Content-type": "application/json" },
 });
