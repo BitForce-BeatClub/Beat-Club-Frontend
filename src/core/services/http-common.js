7ae8ca7b51305e0eb31fbb6be158095c393ab504
adrianesqueiros <u201718169@upc.edu.pe>
2022-06-28 19:37:45 -0500

    firebase.json

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://localhost:8080/api/v1",
   // baseURL: "http://localhost:5100/api/v1",
-  baseURL: "http://localhost:5100/api/v1",
+  baseURL: "https://my-json-server.typicode.com/BitForce-BeatClub/Beat-Club-Frontend",
   headers: {
     "Content-Type": "application/json",
   },
